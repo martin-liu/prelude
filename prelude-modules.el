24b05b7ff339b06f4cef9549a7770c3fd41e64c5
Martin Liu <hflh1989@gmail.com>
2014-05-22 15:14:22 +0800

    not ignore prelude-modules.el

---
+++ prelude-modules.el
@@ -0,0 +1,36 @@
+;;; Uncomment the modules you'd like to use and restart Prelude afterwards
+
+(require 'prelude-programming)
+(require 'prelude-c)
+(require 'prelude-clojure)
+(require 'prelude-coffee)
+;; (require 'prelude-common-lisp)
+(require 'prelude-css)
+(require 'prelude-emacs-lisp)
+(require 'prelude-erc)
+;; (require 'prelude-erlang)
+(require 'prelude-haskell)
+(require 'prelude-js)
+;; (require 'prelude-latex)
+(require 'prelude-lisp)
+(require 'prelude-mediawiki)
+(Require 'prelude-org)
+;; (require 'prelude-perl)
+(require 'prelude-python)
+(require 'prelude-ruby)
+(require 'prelude-scala)
+(require 'prelude-scheme)
+;; (require 'prelude-scss)
+(require 'prelude-shell)
+(require 'prelude-web)
+(require 'prelude-xml)
+
+
+(require 'prelude-helm)
+(require 'prelude-ido)
+(require 'prelude-company) ; auto-complete
+;; (require 'prelude-key-chord)
+
+
+(provide 'prelude-modules)
+;; End of prelude-modules
