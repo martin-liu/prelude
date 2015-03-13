a0a7eddfef7ed5a218df4f3eb33708d29256c24f
Martin Liu <hualiu@ebay.com>
2015-03-13 18:14:56 +0800

    add proxy for linux machine

--- personal/m-config.el
+++ personal/m-config.el
@@ -28,6 +28,7 @@
 (setq-default fill-column 80)
 
 ;;Proxy
-(setq url-proxy-services
-'(("no_proxy" . "localhost")
-  ("http" . "hualiu-9766.lvs01.dev.ebayc3.com:53")))
+(if (equal system-type 'gnu/linux)
+    (setq url-proxy-services
+          '(("no_proxy" . "localhost")
+            ("http" . "hualiu-9766.lvs01.dev.ebayc3.com:53"))))
