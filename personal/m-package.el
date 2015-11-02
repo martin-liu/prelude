d4fcaefa05f3a162788f3fde019e676346d4324f
Martin Liu <hflh1989@gmail.com>
2015-11-02 17:38:35 +0800

    add auto mode for handlerbars/mustache

--- personal/m-package.el
+++ personal/m-package.el
@@ -123,6 +123,8 @@
             (setq web-mode-markup-indent-offset 2)
             (setq web-mode-css-indent-offset 2)
             (setq web-mode-code-indent-offset 2)))
+(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 
 ;;-----------------------------------------------------------
 (provide 'm-package)
